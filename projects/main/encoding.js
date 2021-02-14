847308f9d695b06ae8f801060e7d2f9a112a6136
Thomas <hakerh403@gmail.com>
2021-02-14 13:51:38 +0100

    commit

---
+++ projects/main/encoding.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const assert = require('assert');
+
+class Encoding{
+  static getName(){ O.virtual('getName'); }
+  static checkStr(){ O.virtual('checkStr'); }
+  static countBytes(){ O.virtual('countBytes'); }
+}
+
+class UTF8 extends Encoding{
+  static getName(){ return 'UTF-8'; }
+
+  static checkStr(str){
+    return 1;
+  }
+
+  static countBytes(str){
+    return O.Buffer.from(str).length;
+  }
+}
+
+module.exports = Object.assign(Encoding, {
+  UTF8,
+});
